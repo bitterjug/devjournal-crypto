a27bda27dd6f326ccbe107df0d134b3db9fd3389
Mark Skipper <marks@aptivate.org>
2014-05-20 15:40:21 +0100

    / ‘yemen/Yemen IMP.mm’

--- yemen/Yemen IMP.mm
+++ yemen/Yemen IMP.mm
@@ -7,6 +7,13 @@
 <node CREATED="1400500371205" ID="ID_216367769" MODIFIED="1400500375620" TEXT="both on db"/>
 <node CREATED="1400500376437" ID="ID_1005757792" MODIFIED="1400500380307" TEXT="both after extraction"/>
 </node>
-<node CREATED="1400596181152" ID="ID_595090770" MODIFIED="1400596181152" POSITION="right" TEXT=""/>
+<node CREATED="1400596181152" ID="ID_595090770" MODIFIED="1400596738339" POSITION="right" TEXT="upload">
+<node CREATED="1400596773834" ID="ID_184621229" MODIFIED="1400596783282" TEXT="need to select a bunch of metadata">
+<node CREATED="1400596791594" ID="ID_1464484258" MODIFIED="1400596799810" TEXT="do we assume they have it in the file already?"/>
+<node CREATED="1400596800029" ID="ID_1298236042" MODIFIED="1400596816657" TEXT="does any of it get lined dynamically, e.g. links to other surveys?">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
 </node>
 </map>
