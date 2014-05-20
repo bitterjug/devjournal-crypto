53518f66eab9a2d925a4576f5175e7c2d460a781
Mark Skipper <marks@aptivate.org>
2014-05-20 15:29:39 +0100

    + ‘yemen/Yemen IMP.mm’

---
+++ yemen/Yemen IMP.mm
@@ -0,0 +1,9 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1400500282000" ID="ID_1290774678" MODIFIED="1400500324249" TEXT="Yemen IMP">
+<node CREATED="1400500326871" ID="ID_334010487" MODIFIED="1400500356116" POSITION="right" TEXT="qN and qL data before upload"/>
+<node CREATED="1400500356373" ID="ID_949188688" MODIFIED="1400500369774" POSITION="right" TEXT="qL before upload, qn analysed/compared on db"/>
+<node CREATED="1400500371205" ID="ID_216367769" MODIFIED="1400500375620" POSITION="right" TEXT="both on db"/>
+<node CREATED="1400500376437" ID="ID_1005757792" MODIFIED="1400500380307" POSITION="right" TEXT="both after extraction"/>
+</node>
+</map>
