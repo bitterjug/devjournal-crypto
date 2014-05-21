e8d1c8579ff94d65169b4bdd7ed4fb0577555930
Mark Skipper <marks@aptivate.org>
2014-05-21 15:07:43 +0100

    / ‘yemen/mcs.mm’

--- yemen/mcs.mm
+++ yemen/mcs.mm
@@ -1,4 +1,11 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1400681028638" ID="ID_1891630374" MODIFIED="1400681028638" TEXT="New Mindmap"/>
+<node COLOR="#990000" CREATED="1400681028638" ID="ID_1891630374" MODIFIED="1400681057383" TEXT="mcs">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1400681063027" ID="ID_1461444615" MODIFIED="1400681067114" POSITION="right" TEXT="Tasks">
+<node CREATED="1400681067996" ID="ID_184268945" LINK="https://projects.aptivate.org/issues/5711" MODIFIED="1400681160542" TEXT="v4c: content replicated within site">
+<node CREATED="1400681126769" ID="ID_962608788" MODIFIED="1400681128322" TEXT="We are having problems populating the MIS. [13:24:25] Emeka Nwankwo: The Output 5 results and activities have been replication across the output, and suddenly started replicating in other areas such as M&amp;E [13:24:48] Emeka Nwankwo: How can we quickly address this "/>
+</node>
+</node>
+</node>
 </map>
