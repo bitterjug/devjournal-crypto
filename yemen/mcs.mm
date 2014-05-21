a22bd6e2f622d2e8b9190031514eb215e6515a89
Mark Skipper <marks@aptivate.org>
2014-05-21 16:02:26 +0100

    / ‘yemen/mcs.mm’

--- yemen/mcs.mm
+++ yemen/mcs.mm
@@ -3,20 +3,17 @@
 <node COLOR="#990000" CREATED="1400681028638" ID="ID_1891630374" MODIFIED="1400681057383" TEXT="mcs">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1400681063027" ID="ID_1461444615" MODIFIED="1400681604356" POSITION="right" TEXT="Tasks">
-<node CREATED="1400681067996" ID="ID_184268945" LINK="https://projects.aptivate.org/issues/5711" MODIFIED="1400681160542" TEXT="v4c: content replicated within site">
-<node CREATED="1400681126769" ID="ID_962608788" MODIFIED="1400681128322" TEXT="We are having problems populating the MIS. [13:24:25] Emeka Nwankwo: The Output 5 results and activities have been replication across the output, and suddenly started replicating in other areas such as M&amp;E [13:24:48] Emeka Nwankwo: How can we quickly address this "/>
-</node>
-<node CREATED="1400682497206" ID="ID_1477540388" MODIFIED="1400682499002" TEXT="v4c:">
-<node CREATED="1400682479021" ID="ID_793244376" MODIFIED="1400682484699" TEXT="want separate domain name"/>
-</node>
 <node CREATED="1400682501712" ID="ID_119942923" MODIFIED="1400682503250" TEXT="v4c:">
 <node CREATED="1400682505076" ID="ID_1372132923" MODIFIED="1400682541390" TEXT="want evidence of supprt contract invoice"/>
+<node CREATED="1400683010575" FOLDED="true" ID="ID_1125822838" MODIFIED="1400684443656" TEXT="downloading the quarterly report seems unreliable">
+<node CREATED="1400683034611" ID="ID_202186832" MODIFIED="1400683044131" TEXT="and they didnt know how to do it wouthot a button"/>
+</node>
+<node CREATED="1400682479021" ID="ID_793244376" LINK="https://projects.aptivate.org/issues/5712" MODIFIED="1400683914513" TEXT="want separate domain name"/>
+<node CREATED="1400681067996" FOLDED="true" ID="ID_184268945" LINK="https://projects.aptivate.org/issues/5711" MODIFIED="1400684450909" TEXT="v4c: content replicated within site">
+<node CREATED="1400681126769" ID="ID_962608788" MODIFIED="1400681128322" TEXT="We are having problems populating the MIS. [13:24:25] Emeka Nwankwo: The Output 5 results and activities have been replication across the output, and suddenly started replicating in other areas such as M&amp;E [13:24:48] Emeka Nwankwo: How can we quickly address this "/>
 </node>
 </node>
-<node CREATED="1400681598771" ID="ID_1380001750" MODIFIED="1400681599618" POSITION="right" TEXT="notes">
-<node CREATED="1400681601940" ID="ID_208905809" MODIFIED="1400681603580" TEXT="v4c">
-<node CREATED="1400681627595" ID="ID_1041137087" MODIFIED="1400681654985" TEXT="it takes a LONG time to load the front page now they have added lots of activities"/>
 </node>
-</node>
+<node CREATED="1400681598771" ID="ID_1380001750" MODIFIED="1400681599618" POSITION="right" TEXT="notes"/>
 </node>
 </map>
