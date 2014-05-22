e63651185068afbdb86358987bc8d031f411b4b7
Mark Skipper <marks@aptivate.org>
2014-05-22 11:53:32 +0100

    / ‘yemen/mcs.mm’

--- yemen/mcs.mm
+++ yemen/mcs.mm
@@ -3,7 +3,7 @@
 <node COLOR="#990000" CREATED="1400681028638" ID="ID_1891630374" MODIFIED="1400681057383" TEXT="mcs">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1400681063027" ID="ID_1461444615" MODIFIED="1400681604356" POSITION="right" TEXT="Tasks">
-<node CREATED="1400682501712" FOLDED="true" ID="ID_119942923" MODIFIED="1400686667913" TEXT="v4c:">
+<node CREATED="1400682501712" ID="ID_119942923" MODIFIED="1400755979651" TEXT="v4c:">
 <node CREATED="1400682505076" ID="ID_1372132923" MODIFIED="1400682541390" TEXT="want evidence of supprt contract invoice"/>
 <node CREATED="1400683010575" FOLDED="true" ID="ID_1125822838" LINK="https://projects.aptivate.org/issues/5713" MODIFIED="1400685001480" TEXT="downloading the quarterly report seems unreliable">
 <node CREATED="1400683034611" ID="ID_202186832" MODIFIED="1400683044131" TEXT="and they didnt know how to do it wouthot a button"/>
@@ -17,11 +17,12 @@
 </node>
 </node>
 <node CREATED="1400686687910" ID="ID_1634585377" MODIFIED="1400686690159" TEXT="youwin">
-<node CREATED="1400686700685" ID="ID_1821307632" MODIFIED="1400686737830" TEXT="set up QA snapshots">
+<node CREATED="1400686700685" ID="ID_1821307632" LINK="https://projects.aptivate.org/issues/5682" MODIFIED="1400756008927" TEXT="set up QA snapshots">
 <hook NAME="plugins/TimeManagementReminder.xml">
 <Parameters REMINDUSERAT="1401723480340"/>
 </hook>
 </node>
+<node CREATED="1400755906343" ID="ID_723613994" LINK="https://projects.aptivate.org/issues/5445" MODIFIED="1400755976444" TEXT="Prepare data for MUYI"/>
 </node>
 </node>
 <node CREATED="1400681598771" ID="ID_1380001750" MODIFIED="1400681599618" POSITION="right" TEXT="notes"/>
