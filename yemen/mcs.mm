bfbad9fefbf5c9b3d23c1598f0cbbd4900af71e3
Mark Skipper <marks@aptivate.org>
2014-05-27 11:31:05 +0100

    / ‘yemen/mcs.mm’

--- yemen/mcs.mm
+++ yemen/mcs.mm
@@ -4,20 +4,32 @@
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1400681598771" ID="ID_1380001750" MODIFIED="1400756075819" POSITION="right" TEXT="notes"/>
 <node CREATED="1400681063027" ID="ID_1461444615" MODIFIED="1400681604356" POSITION="right" TEXT="Tasks">
-<node CREATED="1400682501712" FOLDED="true" ID="ID_119942923" MODIFIED="1400756072477" TEXT="v4c:">
-<node CREATED="1400682505076" ID="ID_1372132923" MODIFIED="1400682541390" TEXT="want evidence of supprt contract invoice"/>
-<node CREATED="1400683010575" FOLDED="true" ID="ID_1125822838" LINK="https://projects.aptivate.org/issues/5713" MODIFIED="1400685001480" TEXT="downloading the quarterly report seems unreliable">
+<node CREATED="1400682501712" ID="ID_119942923" MODIFIED="1401186524111" TEXT="v4c:">
+<node CREATED="1400682505076" ID="ID_1372132923" MODIFIED="1400682541390" TEXT="want evidence of supprt contract invoice">
+<node CREATED="1401186534100" ID="ID_170782710" MODIFIED="1401186539196" TEXT="What do you want?">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1400683010575" ID="ID_1125822838" LINK="https://projects.aptivate.org/issues/5713" MODIFIED="1401186542424" TEXT="downloading the quarterly report seems unreliable">
 <node CREATED="1400683034611" ID="ID_202186832" MODIFIED="1400683044131" TEXT="and they didnt know how to do it wouthot a button"/>
+<node CREATED="1401186548220" ID="ID_875642287" MODIFIED="1401186553209" TEXT="but it gives error 500 somtimes"/>
 </node>
 <node CREATED="1400682479021" ID="ID_793244376" LINK="https://projects.aptivate.org/issues/5712" MODIFIED="1400683914513" TEXT="want separate domain name"/>
-<node CREATED="1400681067996" ID="ID_184268945" LINK="https://projects.aptivate.org/issues/5711" MODIFIED="1400684569115" TEXT="v4c: content replicated within site">
+<node CREATED="1401186655491" ID="ID_836709475" MODIFIED="1401186660290" TEXT="per quarter planning view"/>
+<node CREATED="1401186627107" ID="ID_1557309093" MODIFIED="1401186635843" TEXT="improve overall page load performance">
+<node CREATED="1401186638280" ID="ID_336529052" MODIFIED="1401186645552" TEXT="incremental rendering"/>
+<node CREATED="1401186645843" ID="ID_1358719497" MODIFIED="1401186652291" TEXT="only load data for current period"/>
+</node>
+<node CREATED="1401186601636" ID="ID_298643221" MODIFIED="1401186624435" TEXT="filter input text to avoid the &quot;items dupicated&quot; bug"/>
+<node CREATED="1400681067996" ID="ID_184268945" LINK="https://projects.aptivate.org/issues/5711" MODIFIED="1401186598109" TEXT="v4c: content replicated within site">
+<icon BUILTIN="button_ok"/>
 <node CREATED="1400681126769" ID="ID_962608788" MODIFIED="1400681128322" TEXT="We are having problems populating the MIS. [13:24:25] Emeka Nwankwo: The Output 5 results and activities have been replication across the output, and suddenly started replicating in other areas such as M&amp;E [13:24:48] Emeka Nwankwo: How can we quickly address this "/>
 <node CREATED="1400684570315" ID="ID_1970991460" MODIFIED="1400684575166" TEXT="investigate">
 <icon BUILTIN="messagebox_warning"/>
 </node>
 </node>
 </node>
-<node CREATED="1400686687910" ID="ID_1634585377" MODIFIED="1400837435651" TEXT="youwin">
+<node CREATED="1400686687910" FOLDED="true" ID="ID_1634585377" MODIFIED="1401186511527" TEXT="youwin">
 <node CREATED="1400686700685" ID="ID_1821307632" LINK="https://projects.aptivate.org/issues/5682" MODIFIED="1400756008927" TEXT="set up QA snapshots">
 <hook NAME="plugins/TimeManagementReminder.xml">
 <Parameters REMINDUSERAT="1401723480340"/>
