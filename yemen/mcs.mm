cb9c70a8236b485c1bb059e495cdf5e09e37aa54
Mark Skipper <marks@aptivate.org>
2014-06-09 11:17:12 +0100

    / ‘yemen/mcs.mm’

--- yemen/mcs.mm
+++ yemen/mcs.mm
@@ -3,7 +3,26 @@
 <node COLOR="#990000" CREATED="1400681028638" ID="ID_1891630374" MODIFIED="1401962826731" TEXT="mcs">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <icon BUILTIN="male2"/>
-<node CREATED="1400681598771" ID="ID_1380001750" MODIFIED="1401962827388" POSITION="right" TEXT="notes">
+<node CREATED="1400681063027" ID="ID_1461444615" MODIFIED="1402308835891" POSITION="right" TEXT="Tasks">
+<node CREATED="1401197061474" ID="ID_354296444" MODIFIED="1401352466334" TEXT="Yemen ">
+<node CREATED="1401966848004" ID="ID_893361589" MODIFIED="1401966878875" TEXT="Ask Khalid where we can find question text, and what is in the file info file, etc, and about differetn tabs in the sample data file"/>
+</node>
+<node CREATED="1400682501712" ID="ID_119942923" MODIFIED="1402308841001" TEXT="v4c:">
+<node CREATED="1402308902525" ID="ID_1619310459" MODIFIED="1402308911522" TEXT="Iteration starts Wednesday?"/>
+</node>
+<node CREATED="1400686687910" ID="ID_1634585377" MODIFIED="1401980672266" TEXT="youwin">
+<node CREATED="1400755906343" ID="ID_723613994" LINK="https://projects.aptivate.org/issues/5445" MODIFIED="1400755976444" TEXT="Prepare data for MUYI"/>
+<node CREATED="1401960644283" ID="ID_391333664" LINK="https://projects.aptivate.org/issues/5760" MODIFIED="1401960744094" TEXT="Error downloading financial spreadsheet">
+<node CREATED="1402308926493" ID="ID_1373589803" MODIFIED="1402308929384" TEXT="Has this been resolved?"/>
+</node>
+<node CREATED="1401962408327" ID="ID_1448054632" MODIFIED="1401962419696" TEXT="Check time-scale for marking/selecting winners"/>
+<node CREATED="1402309001540" ID="ID_1628113634" MODIFIED="1402309023499" TEXT="Check if they STILL get business plans ithout places to enter stuff (other than when their locks are all spent)"/>
+</node>
+<node CREATED="1402308943540" ID="ID_758581092" MODIFIED="1402308946282" TEXT="office">
+<node CREATED="1402308953165" ID="ID_518811752" MODIFIED="1402308980979" TEXT="Call John Mceleney"/>
+</node>
+</node>
+<node CREATED="1400681598771" ID="ID_1380001750" MODIFIED="1401962827388" POSITION="left" TEXT="notes">
 <node CREATED="1401352479410" FOLDED="true" ID="ID_330107255" MODIFIED="1401982448063" TEXT="Yemen">
 <node CREATED="1401352830377" FOLDED="true" ID="ID_1925615670" MODIFIED="1401962827756" TEXT="workflows">
 <node CREATED="1401352498324" FOLDED="true" ID="ID_189021974" MODIFIED="1401962826463" TEXT="augment">
@@ -22,7 +41,7 @@
 <node CREATED="1401352798546" ID="ID_54997324" MODIFIED="1401352820500" TEXT="search/filter"/>
 <node CREATED="1401352508474" ID="ID_767357108" MODIFIED="1401352509443" TEXT="compare"/>
 <node CREATED="1401352509723" ID="ID_529491266" MODIFIED="1401352786993" TEXT="download">
-<arrowlink DESTINATION="ID_160754124" ENDARROW="Default" ENDINCLINATION="46;0;" ID="Arrow_ID_356209759" STARTARROW="None" STARTINCLINATION="46;0;"/>
+<arrowlink DESTINATION="ID_160754124" ENDARROW="Default" ENDINCLINATION="46;0;" ID="Arrow_ID_1621755756" STARTARROW="None" STARTINCLINATION="46;0;"/>
 </node>
 </node>
 </node>
@@ -47,57 +66,13 @@
 </node>
 </node>
 </node>
-<node CREATED="1401982435175" ID="ID_595397692" MODIFIED="1401982436883" TEXT="youwin">
+<node CREATED="1401982435175" FOLDED="true" ID="ID_595397692" MODIFIED="1402308834862" TEXT="youwin">
 <node CREATED="1401982437685" ID="ID_1818519181" MODIFIED="1401982443738" TEXT="Servers going forward">
 <node CREATED="1401982449302" ID="ID_1800802744" MODIFIED="1401982497716" TEXT="Charge fair price for servers goiong forward"/>
 </node>
 <node CREATED="1401982579652" ID="ID_1137462074" MODIFIED="1401982903364" TEXT="the original contract is a mistake"/>
 <node CREATED="1401982903906" ID="ID_1385190148" MODIFIED="1401982913983" TEXT="we&apos;re expecting to turn off 3 of the servers"/>
 <node CREATED="1401982915202" ID="ID_1054395746" MODIFIED="1401982947584" TEXT="if not, we need to charge complex for the competition + simple for M&amp;E = 7k"/>
-</node>
-</node>
-<node CREATED="1400681063027" FOLDED="true" ID="ID_1461444615" MODIFIED="1401982428143" POSITION="left" TEXT="Tasks">
-<node CREATED="1401197061474" ID="ID_354296444" MODIFIED="1401352466334" TEXT="Yemen ">
-<node CREATED="1401966848004" ID="ID_893361589" MODIFIED="1401966878875" TEXT="Ask Khalid where we can find question text, and what is in the file info file, etc, and about differetn tabs in the sample data file"/>
-</node>
-<node CREATED="1400682501712" FOLDED="true" ID="ID_119942923" MODIFIED="1401962995932" TEXT="v4c:">
-<node CREATED="1400682505076" FOLDED="true" ID="ID_1372132923" MODIFIED="1401962827757" TEXT="want evidence of supprt contract invoice">
-<node CREATED="1401186534100" ID="ID_170782710" MODIFIED="1401186539196" TEXT="What do you want?">
-<icon BUILTIN="help"/>
-</node>
-</node>
-<node CREATED="1400683010575" FOLDED="true" ID="ID_1125822838" LINK="https://projects.aptivate.org/issues/5713" MODIFIED="1401962827757" TEXT="downloading the quarterly report seems unreliable">
-<node CREATED="1400683034611" ID="ID_202186832" MODIFIED="1400683044131" TEXT="and they didnt know how to do it wouthot a button"/>
-<node CREATED="1401186548220" ID="ID_875642287" MODIFIED="1401186553209" TEXT="but it gives error 500 somtimes"/>
-</node>
-<node CREATED="1400682479021" ID="ID_793244376" LINK="https://projects.aptivate.org/issues/5712" MODIFIED="1400683914513" TEXT="want separate domain name"/>
-<node CREATED="1401186655491" ID="ID_836709475" MODIFIED="1401186660290" TEXT="per quarter planning view"/>
-<node CREATED="1401186627107" FOLDED="true" ID="ID_1557309093" MODIFIED="1401962827758" TEXT="improve overall page load performance">
-<node CREATED="1401186638280" ID="ID_336529052" MODIFIED="1401186645552" TEXT="incremental rendering"/>
-<node CREATED="1401186645843" ID="ID_1358719497" MODIFIED="1401186652291" TEXT="only load data for current period"/>
-</node>
-<node CREATED="1401186601636" ID="ID_298643221" MODIFIED="1401186624435" TEXT="filter input text to avoid the &quot;items dupicated&quot; bug"/>
-<node CREATED="1400681067996" FOLDED="true" ID="ID_184268945" LINK="https://projects.aptivate.org/issues/5711" MODIFIED="1401962827758" TEXT="v4c: content replicated within site">
-<icon BUILTIN="button_ok"/>
-<node CREATED="1400681126769" ID="ID_962608788" MODIFIED="1400681128322" TEXT="We are having problems populating the MIS. [13:24:25] Emeka Nwankwo: The Output 5 results and activities have been replication across the output, and suddenly started replicating in other areas such as M&amp;E [13:24:48] Emeka Nwankwo: How can we quickly address this "/>
-<node CREATED="1400684570315" ID="ID_1970991460" MODIFIED="1400684575166" TEXT="investigate">
-<icon BUILTIN="messagebox_warning"/>
-</node>
-</node>
-<node CREATED="1401196796724" ID="ID_1621174340" MODIFIED="1401196816004" TEXT="Need to be able to export planning reports to &quot;snapshot&quot;"/>
-<node CREATED="1401187281063" FOLDED="true" ID="ID_194296368" MODIFIED="1401962827759" TEXT="FIXING:">
-<node CREATED="1401187291415" ID="ID_313784850" MODIFIED="1401187293025" TEXT="how soon?"/>
-</node>
-</node>
-<node CREATED="1400686687910" ID="ID_1634585377" MODIFIED="1401980672266" TEXT="youwin">
-<node CREATED="1400686700685" ID="ID_1821307632" LINK="https://projects.aptivate.org/issues/5682" MODIFIED="1401811321795" TEXT="set up QA snapshots"/>
-<node CREATED="1400755906343" ID="ID_723613994" LINK="https://projects.aptivate.org/issues/5445" MODIFIED="1400755976444" TEXT="Prepare data for MUYI"/>
-<node CREATED="1401960644283" ID="ID_391333664" LINK="https://projects.aptivate.org/issues/5760" MODIFIED="1401960744094" TEXT="Error downloading financial spreadsheet"/>
-<node CREATED="1400837439011" ID="ID_1635038498" MODIFIED="1400837451350" TEXT="anthea&apos;s email about future iterations -- what is the contrac sitiation?">
-<icon BUILTIN="help"/>
-</node>
-<node CREATED="1401197052146" ID="ID_37459345" MODIFIED="1401960635701" TEXT="call Muyi"/>
-<node CREATED="1401962408327" ID="ID_1448054632" MODIFIED="1401962419696" TEXT="Check time-scale for marking/selecting winners"/>
 </node>
 </node>
 </node>
